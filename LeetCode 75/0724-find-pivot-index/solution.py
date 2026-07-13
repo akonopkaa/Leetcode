31b48b637eccefab365c9277ab474e7f5f6dfb07
Aleksander Konopka <aleksanderkonopkaa@icloud.com>
2026-07-13 16:52:38 +0000

    [LeetCode Sync] Runtime - 8 ms (38.92%), Memory - 20.2 MB (81.46%)

---
+++ LeetCode 75/0724-find-pivot-index/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums):
+        sum_right = sum(nums)
+        sum_left = 0
+        for i, num in enumerate(nums):
+            sum_right -= num
+            if sum_left == sum_right:
+                return i
+            sum_left += num
+        return -1
