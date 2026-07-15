d09e46ffe2022b46f73576a98a5b754132d850ff
Aleksander Konopka <aleksanderkonopkaa@icloud.com>
2026-07-15 17:26:08 +0000

    [LeetCode Sync] Runtime - 84 ms (88.77%), Memory - 20.6 MB (18.92%)

---
+++ LeetCode 75/2470-removing-stars-from-a-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s):
+        stack = []
+        for char in s:
+            if char != "*":
+                stack.append(char)
+            else:
+                stack.pop()
+        return "".join(stack)
